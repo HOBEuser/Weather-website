07ba255821f5e77d2b01dd74f75c71db291fa2b5
HOBEuser <166417452+HOBEuser@users.noreply.github.com>
2025-02-21 13:59:17 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-document.getElementById('show-radar').addEventListener('click', function () {
+
     const iframe = document.createElement('iframe');
     iframe.src = "https://www.rainviewer.com/map.html?type=radar&lat=40.7128&lon=-74.0060&zoom=8&opacity=0.8&basemap=1";
     iframe.width = "100%";
@@ -10,4 +10,3 @@ document.getElementById('show-radar').addEventListener('click', function () {
     const container = document.getElementById('radar-container');
     container.innerHTML = "";
     container.appendChild(iframe);
-});
